9e24d5e784e9995438369a3f840196339ee913ec
blinovmaxim <blinov.maxim@gmail.com>
2022-09-23 21:33:10 +0300

    first commit

---
+++ ecomm/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+from django.urls import reverse
+from colorfield.fields import ColorField
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Наименование Категории')
+    slug = models.SlugField(max_length=100, db_index=True, unique=True, verbose_name='URL')
+    description = models.TextField(max_length=1000, blank=True, verbose_name='Описание')
+    available = models.BooleanField(default=True, verbose_name='Опубликован')
+    parent = TreeForeignKey("self", on_delete=models.CASCADE, related_name='children', null=True, blank=True,
+                            verbose_name='Родительская Категория')
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+        # level_attr = 'mptt_level'
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f'{self.name} | - {self.parent}' if self.parent else self.name
+
+    def get_absolute_url(self):
+        return reverse('ecomm:product_list_by_category', args=[self.slug])
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=150, db_index=True, verbose_name='Product Name')
+    available = models.BooleanField(default=True, verbose_name='Опубликован')
+
+    class Meta:
+        verbose_name = 'Product Type'
+        verbose_name_plural = 'Product Types'
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSpecification(models.Model):
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=150, db_index=True, verbose_name='Name')
+
+    class Meta:
+        verbose_name = 'Product Specification'
+        verbose_name_plural = 'Product Specifications'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    product_type = models.ForeignKey(ProductType, on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    name = models.CharField(max_length=150, db_index=True, verbose_name='Наименование')
+    slug = models.SlugField(max_length=150, db_index=True, unique=True, verbose_name='URL')
+    image = models.ImageField(upload_to="product/%Y", blank=True, verbose_name="Фото")
+    description = models.TextField(max_length=1000, blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    quantity = models.IntegerField(verbose_name='Количество', default=20)
+    available = models.BooleanField(default=True, verbose_name='Опубликован')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    uploaded = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('ecomm:product_detail', args=[self.id, self.slug])
+
+    def image_img(self):
+        if self.image:
+            from django.utils.safestring import mark_safe
+            return mark_safe(u'<a href="{0}" target="_blank"><img src="{0}" width="100"/></a>'.format(self.image.url))
+        else:
+            return '(Нет изображения)'
+
+    image_img.short_description = 'Картинка'
+    image_img.allow_tags = True
+
+
+class ProductSpecificationValue(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    specification = models.ForeignKey(ProductSpecification, on_delete=models.RESTRICT)
+    value = models.CharField(max_length=255, verbose_name='Value')
+
+    class Meta:
+        verbose_name = 'Product Specification Value'
+        verbose_name_plural = 'Product Specifications Values'
+
+    def __str__(self):
+        return self.value
---
+++ ecomm/views.py
@@ -0,0 +1,63 @@
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from cart.form import CartAddProductForm
+
+
+menu = [{'title': "О сайте", 'url_name': '/about'},
+        {'title': "Обратная связь", 'url_name': '/feedback'},
+        {'title': "Что-то ещё", 'url_name': '/something'},
+        {'title': "Корзина", 'url_name': '/cart'},
+        {'title': "Войти", 'url_name': '/login'},
+        ]
+
+
+def homepage(request, cat_slug=None):
+    category = None
+    categories = Category.objects.all()
+    # categories = Category.objects.filter(level=0)
+    # categories = Category.get_children()
+
+    products = Product.objects.filter(available=True)
+    if cat_slug:
+        category = get_object_or_404(categories, slug=cat_slug)
+        products = products.filter(category=category)
+    context = {'menu': menu,
+               'products': products,
+               'category': category,
+               'categories': categories,
+               'title': 'Главная страница',
+               }
+    return render(request, 'ecomm/main.html', context=context)
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    product_spec = ProductSpecification.objects.all()
+    product_spec_val = ProductSpecificationValue.objects.all()
+    cart_product_form = CartAddProductForm()
+    context = {'menu': menu,
+               'product_spec': product_spec,
+               'product_spec_val': product_spec_val,
+               'product': product,
+               'cart_product_form': cart_product_form}
+
+    return render(request, 'ecomm/product.html', context=context)
+
+
+def feedback(request):
+    return render(request, 'ecomm/feedback.html', {'menu': menu, 'title': 'Обратная связь'})
+
+
+def login(request):
+    return render(request, 'ecomm/login.html', {'menu': menu, 'title': 'Личный кабинет'})
+
+
+def something(request):
+    return render(request, 'ecomm/something.html', {'menu': menu, 'title': 'Что то полезное'})
+
+
+def about(request):
+    return render(request, 'ecomm/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
