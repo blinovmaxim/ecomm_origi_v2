454629916600a9be75ad8e0244df4fa6a1675e32
blinovmaxim <blinov.maxim@gmail.com>
2022-09-25 20:54:00 +0300

    1

--- shop/settings.py
+++ shop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beetrootshop.herokuapp.com']
+ALLOWED_HOSTS = ['beetrootshop.herokuapp.com','127.0.0.1']
 
 # Application definition
 
